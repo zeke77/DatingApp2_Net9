df4a3514ee72382e609283dd3a97947e88f47117
steven mandel <smandel7@hotmail.com>
2025-07-23 12:14:16 -0400

    End of section 6

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,71 +1,13 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { lastValueFrom } from 'rxjs';
-import { Home } from "../features/home/home";
-import { User } from '../types/user';
-// import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [Nav, Home]
-})
-export class App implements OnInit {
-
-  private accountService = inject(AccountService)
-   private http = inject(HttpClient) ;
-  
-  //protected readonly title = signal('client');
-  protected readonly title = 'Dating App';
-
-protected members = signal<User[]>([]);
-
-
-  //  ngOnInit(): void {
-   
-  //   var url = 'https://localhost:5001/api/members';
-    
-  //   this.http.get(url).subscribe({
-  //     next: response => this.members.set(response)  ,
-  //     error: error => console.log(error),
-  //     complete: () => console.log("Completed http request")
-  //   });
-
-  // }
-
- async ngOnInit() {
-
-  this.members.set(await this.getMembers());
-  // console.log(this.members());
-  this.setCurrentUser();
-}
-
-setCurrentUser()
-{
-const userString = localStorage.getItem('user');
-if ( !userString) return;
-
-const user = JSON.parse(userString);
-this.accountService.currentUser.set(user);
-}
-
-async getMembers() {
-
- const url = 'https://localhost:5001/api/members';
-
-  try {
-     return lastValueFrom(this.http.get<User[]>(url));
-  } 
-  catch (error)
-   {
-    console.log(error);
-    throw error;
-  }
-  
-  
-}
-
+  imports: [Nav, RouterOutlet]
+ })
+export class App  {
+   protected router = inject(Router) ;
 }
--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,36 +1,45 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
-  styleUrl: './nav.css'
+  styleUrl: './nav.css',
 })
 export class Nav {
-  protected accountService = inject(AccountService)
-protected creds: any = {}
+  protected accountService = inject(AccountService);
+  private router = inject(Router);
+  private toast = inject(ToastService);
+  protected creds: any = {};
 
+  login() {
+    // alert("In login");
+    //console.log(this.creds);
 
-login() {
-  // alert("In login");
-  //console.log(this.creds);
+    this.accountService.login(this.creds).subscribe({
+      next: () => {
+        //console.log(result);
+        this.router.navigateByUrl('/members');
+        this.toast.success('Logged in successfully');
+        this.creds = {};
+      },
+      // error: (error) => alert(error.message),  
+      error: error => {
+        //console.log(error);
+        this.toast.error(error.error);
+      }
 
-  this.accountService.login(this.creds).subscribe({
-    next: result => {
-      console.log(result);
-      
-      this.creds = {};
-    },
-    error: error => alert(error.message)
-      
-    })
-  
+    });
+  }
+
+  logout() {
+    
+     this.router.navigateByUrl('/');
+
+    this.accountService.logout();
+  }
 }
-
-logout() {
-  this.accountService.logout();
-    }
-}
-
